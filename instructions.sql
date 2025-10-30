95dc5ca3136a4b8f7fd8f9c35daafe7c4424bbd9
kamalou7 <kamalouhni192@gmail.com>
2025-10-30 21:33:30 +0100

    Ajout du projet Netflix Data Analysis

---
+++ instructions.sql
@@ -0,0 +1,142 @@
+USE ROLE ACCOUNTADMIN;
+
+
+CREATE ROLE IF NOT EXISTS TRANSFORM;
+GRANT ROLE TRANSFORM TO ROLE ACCOUNTADMIN;
+
+
+CREATE WAREHOUSE IF NOT EXISTS COMPUTE_WH;
+GRANT OPERATE ON WAREHOUSE COMPUTE_WH TO ROLE TRANSFORM;
+
+
+CREATE USER IF NOT EXISTS dbt
+  PASSWORD='dbtPassword123'
+  LOGIN_NAME='dbt'
+  MUST_CHANGE_PASSWORD=FALSE
+  DEFAULT_WAREHOUSE='COMPUTE_WH'
+  DEFAULT_ROLE=TRANSFORM
+  DEFAULT_NAMESPACE='MOVIELENS.RAW'
+  COMMENT='DBT user used for data transformation';
+ALTER USER dbt SET TYPE = LEGACY_SERVICE;
+GRANT ROLE TRANSFORM TO USER dbt;
+
+
+CREATE DATABASE IF NOT EXISTS MOVIELENS;
+CREATE SCHEMA IF NOT EXISTS MOVIELENS.RAW;
+
+
+GRANT ALL ON WAREHOUSE COMPUTE_WH TO ROLE TRANSFORM;
+GRANT ALL ON DATABASE MOVIELENS TO ROLE TRANSFORM;
+GRANT ALL ON ALL SCHEMAS IN DATABASE MOVIELENS TO ROLE TRANSFORM;
+GRANT ALL ON FUTURE SCHEMAS IN DATABASE MOVIELENS TO ROLE TRANSFORM;
+GRANT ALL ON ALL TABLES IN SCHEMA MOVIELENS.RAW TO ROLE TRANSFORM;
+GRANT ALL ON FUTURE TABLES IN SCHEMA MOVIELENS.RAW TO ROLE TRANSFORM;
+
+CREATE STAGE netflixstage
+  URL='s3://netflixmoviedata' 
+  CREDENTIALS=(AWS_KEY_ID='I am not crazy for putting it on my GitHub' AWS_SECRET_KEY='haha');
+
+
+USE WAREHOUSE COMPUTE_WH;
+USE DATABASE MOVIELENS;
+USE SCHEMA RAW;
+
+
+CREATE OR REPLACE TABLE raw_movies (
+  movieId INTEGER,
+  title STRING,
+  genres STRING
+);
+
+
+COPY INTO raw_movies
+FROM '@netflixstage/movies.csv'
+FILE_FORMAT = (TYPE = 'CSV' SKIP_HEADER = 1 FIELD_OPTIONALLY_ENCLOSED_BY = '"');
+
+
+CREATE OR REPLACE TABLE raw_ratings (
+  userId INTEGER,
+  movieId INTEGER,
+  rating FLOAT,
+  timestamp BIGINT
+);
+
+COPY INTO raw_ratings
+FROM '@netflixstage/ratings.csv'
+FILE_FORMAT = (TYPE = 'CSV' SKIP_HEADER = 1 FIELD_OPTIONALLY_ENCLOSED_BY = '"');
+
+
+CREATE OR REPLACE TABLE raw_tags (
+  userId INTEGER,
+  movieId INTEGER,
+  tag STRING,
+  timestamp BIGINT
+);
+
+COPY INTO raw_tags
+FROM '@netflixstage/tags.csv'
+FILE_FORMAT = (TYPE = 'CSV' SKIP_HEADER = 1 FIELD_OPTIONALLY_ENCLOSED_BY = '"')
+ON_ERROR = 'CONTINUE';
+
+
+CREATE OR REPLACE TABLE raw_genome_scores (
+  movieId INTEGER,
+  tagId INTEGER,
+  relevance FLOAT
+);
+
+COPY INTO raw_genome_scores
+FROM '@netflixstage/genome-scores.csv'
+FILE_FORMAT = (TYPE = 'CSV' SKIP_HEADER = 1 FIELD_OPTIONALLY_ENCLOSED_BY = '"');
+
+
+CREATE OR REPLACE TABLE raw_genome_tags (
+  tagId INTEGER,
+  tag STRING
+);
+
+COPY INTO raw_genome_tags
+FROM '@netflixstage/genome-tags.csv'
+FILE_FORMAT = (TYPE = 'CSV' SKIP_HEADER = 1 FIELD_OPTIONALLY_ENCLOSED_BY = '"');
+
+
+CREATE OR REPLACE TABLE raw_links (
+  movieId INTEGER,
+  imdbId INTEGER,
+  tmdbId INTEGER
+);
+
+COPY INTO raw_links
+FROM '@netflixstage/links.csv'
+FILE_FORMAT = (TYPE = 'CSV' SKIP_HEADER = 1 FIELD_OPTIONALLY_ENCLOSED_BY = '"');
+
+-- set environment
+virtualenv venv
+venv\Scripts\activate
+
+-- install dbt snowflake
+pip install dbt-snowflake==1.9.0
+
+-- create dbt profile
+mkdir ~/.dbt
+
+-- windows 
+mkdir %userprofile%\.dbt
+
+#initiate dbt project 
+dbt init dbt_project
+
+-- Add staging tables -> all will be views
+-- Update dbt_project.yaml file to support of tables
+dim:
+  +materialized: table
+--   +schema: dim
+-- fct:
+--   +materialized: incremental
+--   +schema: fct
+-- mart:
+--   +materialized: table
+--   +schema: mart
+-- snapshots:
+--   +schema: snapshots
+
