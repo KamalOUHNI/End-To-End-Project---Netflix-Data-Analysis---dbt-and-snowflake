21f7630259b0c35f5f76bfee10d55919542b7406
Kamal OUHNI <167894625+KamalOUHNI@users.noreply.github.com>
2025-10-30 21:35:46 +0100

    Change dbt_project.yaml
    Removed commented-out configuration for dbt project.

--- instructions.sql
+++ instructions.sql
@@ -126,17 +126,3 @@ mkdir %userprofile%\.dbt
 #initiate dbt project 
 dbt init dbt_project
 
--- Add staging tables -> all will be views
--- Update dbt_project.yaml file to support of tables
-dim:
-  +materialized: table
---   +schema: dim
--- fct:
---   +materialized: incremental
---   +schema: fct
--- mart:
---   +materialized: table
---   +schema: mart
--- snapshots:
---   +schema: snapshots
-
